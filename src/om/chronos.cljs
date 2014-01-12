d02f70f347806dae554885d2c8b963c8e415abe5
David Nolen <david.nolen@gmail.com>
2014-01-12 14:33:01 -0500

    sketching things out

---
+++ src/om/chronos.cljs
@@ -0,0 +1,23 @@
+(ns om.chronos)
+
+(defprotocol IAppHistory
+  (-record [this])
+  (-stop [this]))
+
+(defprotocol IAppHistoryTree
+  (-commit [this])
+  (-branch [this name])
+  (-merge [this state]))
+
+(defprotocol IAppTimeline
+  (-previous [this])
+  (-next [this])
+  (-switch [this name]))
+
+(defprotocol IStep
+  (-forward [this] [this steps])
+  (-back [this] [this steps]))
+
+(defprotocol IBookmark
+  (-bookmark [this name])
+  (-jump [this name]))
