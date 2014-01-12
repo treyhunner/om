57cab4bf8c147dae646189f8ff1c52650941dd83
David Nolen <david.nolen@gmail.com>
2014-01-12 16:42:40 -0500

    wip

--- src/om/chronos.cljs
+++ src/om/chronos.cljs
@@ -1,15 +1,15 @@
 (ns om.chronos)
 
-(defprotocol IAppHistory
+(defprotocol IHistory
   (-record [this])
   (-stop [this]))
 
-(defprotocol IAppHistoryTree
+(defprotocol IHistoryTree
   (-commit [this])
   (-branch [this name])
   (-merge [this state]))
 
-(defprotocol IAppTimeline
+(defprotocol ITimeline
   (-previous [this])
   (-next [this])
   (-switch [this name]))
@@ -22,3 +22,36 @@
   (-bookmark [this name])
   (-jump [this name])
   (-list [this]))
+
+;; =============================================================================
+;; API
+
+(defn record [control]
+  (-record control))
+
+;; =============================================================================
+;; SimpleControl
+
+(deftype SimpleControl [app-state history ^:mutable idx tree bookmarks pred key]
+  IHistory
+  (-record [_]
+    (add-watch app-state key
+      (fn [_ _ old new]
+        (when (pred old new)
+          (swap! history conj new)))))
+  (-stop [_] (remove-watch app-state key))
+  IStep
+  (-forward [_]
+    (set! idx (inc idx))
+    (reset! app-state (nth history idx)))
+  (-back [_]
+    (set! idx (dec idx))
+    (reset! app-state (nth history idx))))
+
+(defn simple-control
+  ([app-state] (simple-control app-state (fn [old new] true)))
+  ([app-state pred]
+    (SimpleControl.
+      app-state (atom [@app-state]) 0 (atom (sorted-map)) (atom {})
+      pred (gensym))))
+
